5a2a336589eaaad13a3a01907fa6d7fb93a1d440
Alberto Delicado Delicado <45593059+exalbertod@users.noreply.github.com>
2025-06-04 20:58:49 +0200

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
 import { Link } from "react-router-dom";
+import Favorites from "./Favorites";
 
-export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-black">
+            <div className="container">
+                <Link to="/" className="navbar-brand text-warning">
+                    Galactic Archives
+                </Link>
+                <Favorites />
+            </div>
+        </nav>
+    );
 };
+
+export default Navbar;
